5bfd1b430771888a2df05889ef0f9b1803de2fef
VLADIMIR GONCHAROV <exehot@gmail.com>
2020-06-24 18:39:00 +0300

    stories

--- instagram.js
+++ instagram.js
@@ -9,20 +9,33 @@ export const getUserStories = (username, onReady, repeat = 3) => {
             if (info.graphql && info.graphql.user) {
                 console.log("user id", info.graphql.user.id);
 
+                let headers = {
+                    authority: "api.insta-stories.ru",
+                    pragma: "no-cache",
+                    accept: "application/json, text/plain, */*",
+                    dnt: "1",
+                    origin: "https://insta-stories.ru",
+                    referer: "https://insta-stories.ru/honey_cot"
+                };
+                headers["cache-control"] = "no-cache";
+                headers["user-agent"] =
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36";
+                headers["content-type"] = "application/json";
+                headers["sec-fetch-site"] = "same-site";
+                headers["sec-fetch-mode"] = "cors";
+                headers["sec-fetch-dest"] = "empty";
+                headers["accept-language"] =
+                    "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7";
+
+                let data = {};
+                data["pk"] = info.graphql.user.id;
+                data["x-trip"] = "7gd5a6a17e";
+
                 axios
-                    .post(
-                        "https://api.insta-stories.ru/profile",
-                        {
-                            pk: info.graphql.user.id
-                        },
-                        {
-                            headers: {
-                                origin: "https://insta-stories.ru",
-                                referer: "https://insta-stories.ru/" + username
-                            },
-                            timeout: 5000
-                        }
-                    )
+                    .post("https://api.insta-stories.ru/profile", data, {
+                        headers: headers,
+                        timeout: 8000
+                    })
                     .then(function(stories_data) {
                         onReady(stories_data.data);
                     })
