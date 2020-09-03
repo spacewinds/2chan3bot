a59e7febbcdf7794df150ba6df27d179436df5b8
VLADIMIR GONCHAROV <exehot@gmail.com>
2020-09-03 14:02:56 +0300

    insta stories to dp

--- instagram.js
+++ instagram.js
@@ -1,32 +1,82 @@
 const axios = require("axios");
 const Instagram = require("instagram-web-api");
 let xmlParser = require("xml2json");
-const client = new Instagram({ username: "username", password: "password" });
+var html2json = require("html2json").html2json;
 
-/*
-export const getInstaUser = async (username, callback) => {
-    const instagramUser = await client.getUserByUsername({ username });
-    console.log("insta user", instagramUser);
-    if (callback) callback(instagramUser);
+function findNode(id, currentNode) {
+    var i, currentChild, result;
+    result = [];
+
+    if (id == currentNode.class) {
+        result.push(currentNode);
+    } else {
+        Object.keys(currentNode).forEach(key => {
+            currentChild = currentNode[key];
+            if (typeof currentChild === "object") {
+                const innerData = findNode(id, currentChild);
+                innerData.forEach(item => {
+                    result.push(item);
+                });
+            }
+        });
+    }
+    return result;
+}
+
+function get_url_extension(url) {
+    return url
+        .split(/[#?]/)[0]
+        .split(".")
+        .pop()
+        .trim();
+}
+
+function instaDPFormat(data) {
+    let result = [];
+    data.forEach(item => {
+        if (get_url_extension(item.href).toLowerCase() === "jpg") {
+            result.push({ img: { src: item.href } });
+        } else {
+            result.push({ video: { src: item.href } });
+        }
+    });
+    return result;
+}
+
+export const getUserStories_DP = (user, onReady) => {
+    axios
+        .get(`https://www.instadp.com/stories/${user}`)
+        .then(data => {
+            let object = html2json(data.data);
+            let jsons = JSON.stringify(object);
+            try {
+                const result = instaDPFormat(findNode("download-btn", object));
+                console.log("result");
+                onReady(result);
+            } catch (err) {
+                console.log("insta broken");
+            }
+        })
+        .catch(error => {
+            console.log("ERROR", error);
+        });
 };
 
-getInstaUser("honey_cot");*/
-
 export const getUserStories = (user, onReady) => {
-  axios
-    .get(`https://storiesig.com/stories/${user}`)
-    .then((data) => {
-      let object = JSON.parse(xmlParser.toJson(data.data));
-      try {
-        const result = object.html.body.div.div.section.div.article;
-        onReady(result);
-      } catch (err) {
-        console.log("insta broken");
-      }
-
-      fs.writeFile("data", object);
-    })
-    .catch((error) => {
-      console.log("ERROR", error);
-    });
+    axios
+        .get(`https://storiesig.com/stories/${user}`)
+        .then(data => {
+            let object = JSON.parse(xmlParser.toJson(data.data));
+            try {
+                const result = object.html.body.div.div.section.div.article;
+                onReady(result);
+            } catch (err) {
+                getUserStories_DP(user, onReady);
+                console.log("insta broken");
+            }
+        })
+        .catch(error => {
+            getUserStories_DP(user, onReady);
+            console.log("ERROR", error);
+        });
 };
